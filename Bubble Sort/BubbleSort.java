5f531d2f574e3a43510bac32b4ba7a7fe17c3c24
Do Minh Huy <112995017+I3IN1202@users.noreply.github.com>
2023-05-14 17:07:30 +0700

    Update BubbleSort.java

--- Bubble Sort/BubbleSort.java
+++ Bubble Sort/BubbleSort.java
@@ -1,6 +1,6 @@
-
 package bubble.sort;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -9,45 +9,49 @@ import java.util.Scanner;
  */
 public class BubbleSort {
 
-    static void BubbleSort(int[] a){
-        boolean sort = true;
-        for(int i=0; i < a.length - 1; i++){
-            for (int j=0; j < a.length - 1 - i; j++ ){
-                if (a[j] > a[j + 1]){
+    static void BubbleSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
                     int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
-                    sort = false;
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
-            }   
-        }
-        if (sort){
-            System.out.println("Perfect Array!");
-            return;
+            }
         }
     }
-    
+
+    private static int[] createArrray(int size) {
+        int[] a = new int[size];
+        Random rd = new Random();
+        for (int i = 0; i < size; i++) {
+            a[i] = rd.nextInt(100);
+        }
+        return a;
+    }
+
+    private static void display(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (i < a.length - 1) {
+                System.out.print(a[i] + ", ");
+            } else {
+                System.out.print(a[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter array: ");
+        System.out.print("Enter number of array: ");
         int n = Validation.getInt();
-        int[] arr = new int [n];
-        
-        for (int i=0; i < n; i++){
-            arr[i] = Validation.getInt();
-        } 
-        
-        System.out.println("Array: ");
-        for (int i = 0; i < n; i++){
-            System.out.print(arr[i] + " ");
-        }
-        
-        BubbleSort(arr);
+        int[] a = createArrray(n);
+
+        System.out.println("Unsorted array: ");
+        display(a);
+
         System.out.println("\nSorted Array: ");
-        
-        for (int i = 0; i < n; i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println(" ");
+        BubbleSort(a);
+        display(a);
+
     }
 }
