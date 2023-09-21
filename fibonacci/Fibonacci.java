2ce0b094cae86212cf751390cd2d8c5a8b81027e
Do Minh Huy <112995017+I3IN1202@users.noreply.github.com>
2023-09-21 14:39:39 +0700

    Update Fibonacci.java

--- fibonacci/Fibonacci.java
+++ fibonacci/Fibonacci.java
@@ -10,52 +10,28 @@ import java.util.Scanner;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();   
-        // Step 1: Use recursion to find 45 sequence Fibonacci
-        find45sequenceFibonacci(list, 0, true);
-        // Step 2: Display to screen 45 sequence Fibonacci
-        display(list);
-    }
-
-    // Use recursion to 45 sequence Fibonacci
-    public static void find45sequenceFibonacci(ArrayList<Integer> list, int count, boolean check) {
-      
-        // compare number with 0 or 1
-        if (count == 0 || count == 1) {
-            list.add(count);
-            if(check == false)
-            System.out.println(" The number " + (count + 1) + " : " + list.get(count));
-        } else {
-            int number = list.get(count - 2) + list.get(count - 1);
-            list.add(number);
-            if(check == false)
-            System.out.println(" The number " + (count + 1) + "  : " + list.get(count - 1)
-                    + " + " + list.get(count - 2) + "  = " + list.get(count));
-        }
-        count++;
-        // compare count with 45 to stop recursive
-        if (count == 45) {
-            return;
-        }
-        find45sequenceFibonacci(list, count, check);
+        int[] a = new int[46];
+        fibo(0, a, 45);
+        display(a);
     }
     
+    public static void fibo(int n, int[] a, int end){
+        if (n < 2){
+            a[n] = n;
+        }else{
+            a[n] = a[n-1] + a[n-2];
+        }
+        if(n < end){
+            n++;
+            fibo(n, a, end);
+        }
+    }
     
-    //Display to screen 45 sequence Fibonacci
-    public static void display(ArrayList<Integer> list) {
-          
+    public static void display(int[] a){
         System.out.println("The 45 sequence fibonacci: ");
-        // loop use to access first element to before last element of array
-        for (int i = 0; i < list.size() - 1; i++) {
-            System.out.print(list.get(i) + ", ");
+        for(int i = 0; i < a.length - 1; i++){
+              System.out.print(a[i]+", ");
         }
-        System.out.println(list.get(list.size() - 1));
-        
-        getInput();
-    }
-
-    public static void getInput(){
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
+//       System.out.println((a[a.length-1]));
     }
 }
