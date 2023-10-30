81c29806d6e04ffcc5c1b22bdf8c203505c51773
Do Minh Huy <112995017+I3IN1202@users.noreply.github.com>
2023-10-30 22:15:49 +0700

    Update Main.java

--- student/Main.java
+++ student/Main.java
@@ -17,34 +17,41 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<Student> list = new ArrayList<>();
+        ArrayList<Student> ls = new ArrayList<>();
         Validation validation = new Validation();
         Manage manage = new Manage();
-        list.add(new Student("1", "Pham Ngoc Hoa", "Spring", "java"));
-        list.add(new Student("2", "Do Quang Hiep", "Summer", ".net"));
-        list.add(new Student("3", "Nguyen Xuan Cuong", "Spring", "c/c++"));
+        ls.add(new Student("1", "Pham Hoa", "Spring", "java"));
+        ls.add(new Student("2", "Do Hiep", "Summer", ".net"));
+        ls.add(new Student("3", "Nguyen huy", "Spring", "c/c++"));
+        ls.add(new Student("4", "Pham uyên", "Spring", "java"));
+        ls.add(new Student("5", "Do Quang", "Summer", ".net"));
+        ls.add(new Student("6", "Nguyen vanh", "Spring", "c/c++"));
+        ls.add(new Student("7", "Pham Trường", "Spring", "java"));
+        ls.add(new Student("8", "Do hải", "Summer", ".net"));
+        ls.add(new Student("9", "Nguyen anh", "Spring", "c/c++"));
         
-        int count = 3;
-        while (true){
+        while (true) {
             manage.menu();
             int choice = validation.checkInputLimit(1, 5);
-            switch (choice){
+            switch (choice) {
                 case 1:
-                    manage.createStudent(count, list);
+                    manage.displayAll();
+                    manage.createStudent();
                     break;
-                case 2: 
-                    manage.findAndSort(list);
+                case 2:
+                    manage.displayAll();
+                    manage.findAndSort();
                     break;
                 case 3:
-                    manage.updateOrDelete(count, list);
+                    manage.displayAll();
+                    manage.updateOrDelete();
                     break;
                 case 4:
-                    manage.report(list);
+                    manage.report();
                     break;
                 case 5:
                     return;
-            }        
+            }
         }
     }
-    
 }
